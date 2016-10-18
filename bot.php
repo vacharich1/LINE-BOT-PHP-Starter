9c99f49e61170af0f49f00cf5893f339f9a16323
jay <jr30149_@hotmail.com>
2016-10-18 20:56:22 +0700

    333

--- bot.php
+++ bot.php
@@ -18,15 +18,22 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
+				{
+					'type' => 'text',
+					'text' => $text
+				},
+				{
+					'type' => 'text',
+					'text' => 'hello'
+				}
 
+				
+			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => [$messages]
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
@@ -44,4 +51,4 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "OK";
+echo "OK11";
