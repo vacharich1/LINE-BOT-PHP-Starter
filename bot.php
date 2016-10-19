3156e609dd0c366cad248e790244bd94ea1a64ea
jay <jr30149_@hotmail.com>
2016-10-19 19:54:52 +0700

    xcbxcvxcv

--- bot.php
+++ bot.php
@@ -23,8 +23,10 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = array(['type' => 'text','text' => $text]
-			);
+			$messages = ['type' => 'text',
+				     'text' => $text
+			];
+			
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
