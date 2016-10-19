d695312b8c5e3c27ca740a17c4a5781095c2e7cb
jay <jr30149_@hotmail.com>
2016-10-19 21:22:30 +0700

    cccc

--- bot.php
+++ bot.php
@@ -46,6 +46,15 @@ if (!is_null($events['events'])) {
 			$result = curl_exec($ch);
 			curl_close($ch);
 
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
 			echo $result . "\r\n";
 		}
 	}
