d9317f2f9c899ba8bd5827d85b8296873bd3322b
jay <jr30149_@hotmail.com>
2016-10-20 20:16:51 +0700

    ccccc

--- bot.php
+++ bot.php
@@ -11,35 +11,45 @@ $events = json_decode($content, true);
 
 
 
-$requestBodyObject = json_decode($content);
-$requestContent = $requestBodyObject->result{0}->content;
-$requestText = $requestContent->text; 
-$requestFrom = $requestContent->from; 
-$contentType = $requestContent->contentType; 
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
 
-$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			// Get replyToken
+			$replyToken = $event['replyToken'];
 
-$responseText = 'test'
+			// Build message to reply back
+			$messages = array['type' => 'text',
+				     'text' => $text
+			];
 
-$responseMessage =
-    {
-      "to":["{$requestFrom}"],
-      "toChannel":1383378250,
-      "eventType":"138311608800106203",
-      "content":{
-        "contentType":1,
-        "toType":1,
-        "text":"{$responseText}"
-      }
-    }
+			
 
-$curl = curl_init('https://trialbot-api.line.me/v1/events');
-  curl_setopt($curl, CURLOPT_POST, true);
-  curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-  curl_setopt($curl, CURLOPT_POSTFIELDS, $responseMessage);
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); 
-  curl_setopt($curl, CURLOPT_HTTPPROXYTUNNEL, 1);
-  curl_setopt($curl, CURLOPT_PROXY, getenv('FIXIE_URL'));
-  $output = curl_exec($curl);
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages]
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+	}
+}
 echo "OK11";
