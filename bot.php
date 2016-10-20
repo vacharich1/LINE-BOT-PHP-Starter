cbd400b5f6d6645e8bed584fbb8dff8baf9309a5
jay <jr30149_@hotmail.com>
2016-10-20 21:05:10 +0700

    dddd

--- bot.php
+++ bot.php
@@ -1,4 +1,13 @@
 ﻿<?php
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('AYydB5m2TZasBEFQaZjNRTCTeC3d3oNKw77jzKd/mj3SAMlkABDK74AAJ6eN00no1+MiFoFV2N5pl1KIYZmlq8/WSmxf2b4WVhcvfjJoUH6TY6AZoQrYmAP/ny8krS0KwSMDOokFaUouicUyyIKmhQdB04t89/1O/w1cDnyilFU=');
+$access_token = 'AYydB5m2TZasBEFQaZjNRTCTeC3d3oNKw77jzKd/mj3SAMlkABDK74AAJ6eN00no1+MiFoFV2N5pl1KIYZmlq8/WSmxf2b4WVhcvfjJoUH6TY6AZoQrYmAP/ny8krS0KwSMDOokFaUouicUyyIKmhQdB04t89/1O/w1cDnyilFU=';
+
+// Get POST body content
+$content = file_get_contents('php://input');
+
+
+
+// Parse JSON
+$events = json_decode($content, true);
+
 
 echo 'ok'
