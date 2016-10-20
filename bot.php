6b36f793d3f05b96fd2f9f3f73df15cb6588c81f
jay <jr30149_@hotmail.com>
2016-10-20 21:02:51 +0700

    fff

--- bot.php
+++ bot.php
@@ -1,4 +1,5 @@
-﻿$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('AYydB5m2TZasBEFQaZjNRTCTeC3d3oNKw77jzKd/mj3SAMlkABDK74AAJ6eN00no1+MiFoFV2N5pl1KIYZmlq8/WSmxf2b4WVhcvfjJoUH6TY6AZoQrYmAP/ny8krS0KwSMDOokFaUouicUyyIKmhQdB04t89/1O/w1cDnyilFU=');
+﻿<?php
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('AYydB5m2TZasBEFQaZjNRTCTeC3d3oNKw77jzKd/mj3SAMlkABDK74AAJ6eN00no1+MiFoFV2N5pl1KIYZmlq8/WSmxf2b4WVhcvfjJoUH6TY6AZoQrYmAP/ny8krS0KwSMDOokFaUouicUyyIKmhQdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '3692fbc3db90c226b12e3f91130e2f9f']);
 
 echo 'ok'
