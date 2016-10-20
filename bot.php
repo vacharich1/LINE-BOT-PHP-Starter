318f30e21494bbef855550dace1d1cfa10abc9ae
jay <jr30149_@hotmail.com>
2016-10-20 21:06:57 +0700

    xxxx

--- bot.php
+++ bot.php
@@ -8,6 +8,13 @@ $content = file_get_contents('php://input');
 
 // Parse JSON
 $events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+
+	echo 'ok'
 
 
-echo 'ok'
+}
+
+
+echo "OK11";
