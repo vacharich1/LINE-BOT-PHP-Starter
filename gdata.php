28a4e60c5f8408662c66646bf916540c20d38439
jay <jr30149_@hotmail.com>
2016-10-21 18:46:04 +0700

    ddddd

--- gdata.php
+++
@@ -1,9 +0,0 @@
-﻿<?php     
-header("Content-type:text/html; charset=UTF-8");          
-header("Cache-Control: no-store, no-cache, must-revalidate");         
-header("Cache-Control: post-check=0, pre-check=0", false);         
-if($_GET['rev']==1){  
-    echo date("Y-m-d H:i:s");  
-    exit;  
-}  
-?>  
