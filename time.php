9db52e97a087b891335db8b070f651ffa69b2e3d
jay <jr30149_@hotmail.com>
2016-10-21 18:08:42 +0700

    qqqqq

---
+++ time.php
@@ -0,0 +1,17 @@
+﻿<script type="text/javascript" src="js/jquery-1.4.1.min.js"></script>  
+<script type="text/javascript">  
+$(function(){  
+    setInterval(function(){ // เขียนฟังก์ชัน javascript ให้ทำงานทุก ๆ 30 วินาที  
+        // 1 วินาที่ เท่า 1000  
+        // คำสั่งที่ต้องการให้ทำงาน ทุก ๆ 3 วินาที  
+        var getData=$.ajax({ // ใช้ ajax ด้วย jQuery ดึงข้อมูลจากฐานข้อมูล  
+                url:"gdata.php",  
+                data:"rev=1",  
+                async:false,  
+                success:function(getData){  
+                    $("div#showData").html(getData); // ส่วนที่ 3 นำข้อมูลมาแสดง  
+                }  
+        }).responseText;  
+    },3000);      
+});  
+</script>  
