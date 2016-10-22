dedae19b66234e0a14dd4e18d8c63a7b08b63cf0
jay <jr30149_@hotmail.com>
2016-10-22 12:21:35 +0700

    ddddd

--- bot_test.php
+++ bot_test.php
@@ -1,2 +1,20 @@
-﻿<?php
-echo "I am a bot";
+﻿$HOST_NAME = "http://botlinebyjfourtwins.orgfree.com/";
+	$DB_NAME = "1246984";
+	$CHAR_SET = "charset=utf8"; // เช็ตให้อ่านภาษาไทยได้
+ 
+	$USERNAME = "1246984";     // ตั้งค่าตามการใช้งานจริง
+	$PASSWORD = "p1a6n3g1ya";  // ตั้งค่าตามการใช้งานจริง
+ 
+ 
+	try {
+	
+		$db = new PDO('mysql:host='.$HOST_NAME.';dbname='.$DB_NAME.';'.$CHAR_SET,$USERNAME,$PASSWORD);
+	
+		echo "เชื่อมต่อฐานข้อมูลสำเร็จ";
+	
+	
+	} catch (PDOException $e) {
+	
+		echo "ไม่สามารถเชื่อมต่อฐานข้อมูลได้ : ".$e->getMessage();
+	
+	}
