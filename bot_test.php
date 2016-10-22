00bb53cfe87da2e8e079745d260474bee73fc2d0
jay <jr30149_@hotmail.com>
2016-10-22 16:32:06 +0700

    dddd

--- bot_test.php
+++ bot_test.php
@@ -1,23 +1,25 @@
 ﻿<?php
  
-	$HOST_NAME = "http://botlinebyjfourtwins.orgfree.com/pma/index.php?db=1246984&token=73b82e1c10c93817bdcca80dd457c144";
-	$DB_NAME = "1246984";
+	$HOST_NAME = "sql6.freemysqlhosting.net";
+	$DB_NAME = "sql6141179";
 	$CHAR_SET = "charset=utf8"; // เช็ตให้อ่านภาษาไทยได้
  
-	$USERNAME = "1246984";     // ตั้งค่าตามการใช้งานจริง
-	$PASSWORD = "1234";  // ตั้งค่าตามการใช้งานจริง
+	$USERNAME = "sql6141179";     // ตั้งค่าตามการใช้งานจริง
+	$PASSWORD = "2VSm3JEfdX";  // ตั้งค่าตามการใช้งานจริง
  
  
 	try {
+		
 	
 		$db = new PDO('mysql:host='.$HOST_NAME.';dbname='.$DB_NAME.';'.$CHAR_SET,$USERNAME,$PASSWORD);
 	
-		echo "เชื่อมต่อฐานข้อมูลสำเร็จ";
+		echo "connect";
 	
 	
 	} catch (PDOException $e) {
 	
-		echo "ไม่สามารถเชื่อมต่อฐานข้อมูลได้ : ".$e->getMessage();
+		echo "connot connect".$e->getMessage();
+		echo "assacc";
 	
 	}
  
