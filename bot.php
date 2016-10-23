fc6b8d19932d64b587192c20bc6cbd7ea49427e7
jay <jr30149_@hotmail.com>
2016-10-23 21:25:02 +0700

    ggg

--- bot.php
+++ bot.php
@@ -112,7 +112,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages,$messages1,$messages3]
+				'messages' => [$messages,$messages2]
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
