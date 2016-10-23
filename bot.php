bd6d51d7927430ff1a66d60a1528b606bb69e568
jay <jr30149_@hotmail.com>
2016-10-23 13:16:52 +0700

    dddd

--- bot.php
+++ bot.php
@@ -68,8 +68,8 @@ if (!is_null($events['events'])) {
 			];
 
 			$messages2 = ['type' => 'image',
-				     'originalContentUrl' => 'https://obscure-harbor-99516.herokuapp.com/show.php',
-				     'previewImageUrl' => 'https://obscure-harbor-99516.herokuapp.com/show.php'
+				     'originalContentUrl' => 'https://obscure-harbor-99516.herokuapp.com/golf-ball-clip-art-black-and-white-niX89GjAT1.gif',
+				     'previewImageUrl' => 'https://obscure-harbor-99516.herokuapp.com/golf-ball-clip-art-black-and-white-niX89GjAT1.gif'
 			];
 
 			
