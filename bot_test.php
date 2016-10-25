45162939b28224039b211594955ad6a0f61d19c0
jay <jr30149_@hotmail.com>
2016-10-25 20:39:13 +0700

    ddddd

--- bot_test.php
+++ bot_test.php
@@ -158,7 +158,7 @@ if (!is_null($events['events'])) {
 						$url = 'https://api.line.me/v2/bot/message/reply';
 						$data = [
 							'replyToken' => $replyToken,
-							'messages' => [$messages4]
+							'messages' => [$messages4,$messages5]
 						];
 						$post = json_encode($data);
 						$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
