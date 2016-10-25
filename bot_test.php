8c9845d2608a62712d8dcb9003553de0dd8fcf8e
jay <jr30149_@hotmail.com>
2016-10-25 20:25:05 +0700

    dddd

--- bot_test.php
+++ bot_test.php
@@ -140,7 +140,7 @@ if (!is_null($events['events'])) {
 						else
 							$llll = "https://www.dropbox.com/s/h6yztz70os1ily8/pic.png";
 						// Build message to reply back
-						$messages = ['type' => 'text','text' => $event['source']['groupID']];
+						$messages = ['type' => 'text','text' => $event['message']['id']];
 						//sleep(5);
 						$messages3 = ['type' => 'text','text' => $hoonname];
 			
