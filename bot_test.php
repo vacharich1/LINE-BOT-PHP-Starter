2b1e7b53eb1fe0d36c368eb78e70a25dda3dacf4
jay <jr30149_@hotmail.com>
2016-10-25 21:08:45 +0700

    hhhhh

--- bot_test.php
+++ bot_test.php
@@ -186,6 +186,27 @@ if (!is_null($events['events'])) {
 					}//ifresult<2
 			 }//if ($event['type'] == 'message' && $event['message']['type'] == 'text')
 		}//if($event['source']['groupId'] 
+		else
+		{
+							$url = 'https://api.line.me/v2/bot/message/reply';
+							
+							$messages11 = ['type' => 'text','text' => "รบกวติดต่อ LINE : @JFOURTWINS ครับ ไลน์บอทสามารถใช้ได้เฉพาะห้องที่กำหนดเท่านั้น"];
+							$data = [
+								'replyToken' => $replyToken,
+								'messages' => [$messages4,$messages5]
+							];
+							$post = json_encode($data);
+							$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+				
+							$ch = curl_init($url);
+							curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+							curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+							curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+							curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+							curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+							$result = curl_exec($ch);
+							curl_close($ch);
+		}
 	}//for
 }
 
