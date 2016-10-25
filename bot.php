ba99d026d0501efad6e5d93f14a3547bd44b05c7
jay <jr30149_@hotmail.com>
2016-10-25 17:11:55 +0700

    4444

--- bot.php
+++ bot.php
@@ -60,23 +60,42 @@ $events = json_decode($content, true);
 
 $json = json_decode($content, true);
 
+
+function curlUserProfileFromLine($mid) {
+    $url = 'https://trialbot-api.line.me/v1/profiles?mids=' . $mid;
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'X-Line-ChannelID: ' . LineConfig::getChannelId(),
+        'X-Line-ChannelSecret: ' . LineConfig::getChannelSecret(),
+        'X-Line-Trusted-User-With-ACL: ' . LineConfig::getMID(),
+    ));
+
+    $json_content = curl_exec($ch);
+    curl_close($ch);
+
+    if (!$json_content) {
+        return false;
+    }
+
+    $json = json_decode($json_content, true);
+    foreach ($json['contacts'] as $user) {
+        if ($mid == $user['mid']) {
+            return $user;
+        }
+    }
+
+    return false;
+}
+
 // 可以一次送來多筆資料，所以是陣列
 foreach ($json['result'] as $result) {
     $content = $result['content'];
-    if ($result['eventType'] == '138311609100106403') {
-        // 加入好友或封鎖
-        $mid = $content['params'][0];
-        if ($content['opType'] == 4) {
-            echo '加入好友 ' . $mid;
-        }
-        if ($content['opType'] == 8) {
-            echo '封鎖 ' . $mid;
-        }
-        // 利用 curl 另外取得 user 資料
-        $profile = curlUserProfileFromLine($mid);
-        echo 'name = ' . $profile['displayName'];
-        echo '<img src="' . $profile['pictureUrl'] . '" />';
-    }
+	$mid = $content['params'][0];
+    $profile = curlUserProfileFromLine($mid);
+    echo 'name = ' . $profile['displayName'];
+    echo '<img src="' . $profile['pictureUrl'] . '" />';
+    
 }
 
 
