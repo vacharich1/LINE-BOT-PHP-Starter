9cdc7be74bf038fc1175783eb5dbf34eed2e3710
jay <jr30149_@hotmail.com>
2016-10-29 13:37:57 +0700

    546546546

--- bot_test.php
+++ bot_test.php
@@ -1607,7 +1607,7 @@ if (!is_null($events['events'])) {
 							else
 							{
 								$replyToken = $event['replyToken'];
-								$messages55 = ['type' => 'text','text' => 'error id group'];
+								$messages55 = ['type' => 'text','text' => $event['message']['text']];
 								// Make a POST Request to Messaging API to reply to sender
 								$url = 'https://api.line.me/v2/bot/message/reply';
 								$data = [
