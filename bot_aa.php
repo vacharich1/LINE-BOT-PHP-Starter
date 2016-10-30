4850d32ce63055f6821eec6b2cfa11057577bc81
jay <jr30149_@hotmail.com>
2016-10-30 17:54:36 +0700

    ffff

---
+++ bot_aa.php
@@ -0,0 +1,18 @@
+﻿<?php
+    $host= "http://27.254.82.170:81/phpmyadmin/index.php?old_usr=ideatrad_bot";
+	$db = "ideatrad_bot";
+ 
+	$username = "ideatrad_bot";    
+	$password = "akAtl7B206";   
+	$CHAR_SET = "charset=utf8"; 
+
+	$link = mysqli_connect($host, $username, $password, $db);
+	if (!$link) {
+			echo 'aaaaa';
+    		die('Could not connect: ' . mysqli_connect_errno());
+	}
+	else
+	{
+		echo "connect";
+	}
+?>
