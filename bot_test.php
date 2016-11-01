0838d135cd8deb5786c5a6d8dcb4e357646f88c1
jay <jr30149_@hotmail.com>
2016-11-01 08:21:25 +0700

    finish

--- bot_test.php
+++ bot_test.php
@@ -99,7 +99,7 @@ if (!is_null($events['events'])) {
 							$room='1';
 							if($event['source']['groupId'] == 'C941fb2b8a40f9d0f400969fa848c3386' || $event['source']['groupId'] == 'C26d889d89b336a786c06358c1e2df27c')//graph black
 								$room='2';
-							else if($event['source']['groupId'] == 'C9f2b93574be7434e6e7180a7d7503601' || $event['source']['groupId'] == 'Cd3afd7bd7719ceb0822ea162b50000fb' || $event['source']['groupId'] == 'C7ab92191511e47ff839c174e7f2104c5'|| $event['source']['groupId'] == 'Cb880ba168d17c69174b652045a8f8b90' || $event['source']['groupId'] == 'C1970832b0d5e677dc1498442f8412be7')//graph 1
+							else if($event['source']['groupId'] == 'C9f2b93574be7434e6e7180a7d7503601' || $event['source']['groupId'] == 'Cd3afd7bd7719ceb0822ea162b50000fb' || $event['source']['groupId'] == 'C7ab92191511e47ff839c174e7f2104c5')//graph 1
 								$room='1';
 							else
 								$room='3';
