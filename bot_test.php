d54f5cdb4eccbb1d09b10e36769ac4a456c7aaac
jay <jr30149_@hotmail.com>
2016-11-03 20:17:08 +0700

    aaaa

--- bot_test.php
+++ bot_test.php
@@ -1593,32 +1593,29 @@ if (!is_null($events['events'])) {
 		}//if($event['source']['groupId'] 
 		else
 		{
-				#foreach ($events['events'] as $event) {
-					#$text = $event['message']['text'];
-			
-					#if($text == "@@addgroupbyjay")
-					{
-							$replyToken = $event['replyToken'];
-							$messages55 = ['type' => 'text','text' => $event['source']['groupId']];
-							// Make a POST Request to Messaging API to reply to sender
-							$url = 'https://api.line.me/v2/bot/message/reply';
-							$data = [
-										'replyToken' => $replyToken,
-										'messages' => [$messages55]
-									];
-							$post = json_encode($data);
-							$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+				#if($text == "@@addgroupbyjay")
+				#{
+				$replyToken = $event['replyToken'];
+				$messages55 = ['type' => 'text','text' => $event['source']['groupId']];
+				// Make a POST Request to Messaging API to reply to sender
+				$url = 'https://api.line.me/v2/bot/message/reply';
+				$data = [
+							'replyToken' => $replyToken,
+							'messages' => [$messages55]
+						];
+				$post = json_encode($data);
+				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 									
-							$ch = curl_init($url);
-							curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-							curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-							curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-							curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-							curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-							$result = curl_exec($ch);
-							curl_close($ch);
-					}	
-				}
+				$ch = curl_init($url);
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+				$result = curl_exec($ch);
+				curl_close($ch);
+					#}	
+				#}
 		}
 	}//for
 }
