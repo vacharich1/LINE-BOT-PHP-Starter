a9339eedf3f71853ea6a82f6c75fa31b6d16a319
jay <jr30149_@hotmail.com>
2016-11-05 11:01:07 +0700

    finish add text

--- bot_test.php
+++ bot_test.php
@@ -92,25 +92,7 @@ if (!is_null($events['events'])) {
 						{
 							if($arr1[1] == " ")
 							{
-								$replyToken = $event['replyToken'];
-								$messages55 = ['type' => 'text','text' => "ต้องไม่มีช่องว่างจ๊ะ รบกวนพิมอีกครั้งครับ @ aot 60  -------> @aot 60"];
-								// Make a POST Request to Messaging API to reply to sender
-								$url = 'https://api.line.me/v2/bot/message/reply';
-								$data = [
-									'replyToken' => $replyToken,
-									'messages' => [$messages55]
-								];
-								$post = json_encode($data);
-								$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-					
-								$ch = curl_init($url);
-								curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-								curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-								curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-								curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-								curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-								$result = curl_exec($ch);
-								curl_close($ch);	
+								$hoonname = substr($textcut[0], 2); // cut@
 							}
 							else
 							{
