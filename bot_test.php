b3c9a94c100c4e02ea7a38f4ed54d56f6854e888
jay <jr30149_@hotmail.com>
2016-11-06 13:30:13 +0700

    check when not capture

--- bot_test.php
+++ bot_test.php
@@ -1559,27 +1559,92 @@ if (!is_null($events['events'])) {
 										 'originalContentUrl' => 'http://static.bigstockphoto.com/images/homepage/2016_popular_photo_categories.jpg',
 										 'previewImageUrl' => 'http://static.bigstockphoto.com/images/homepage/2016_popular_photo_categories.jpg'
 								];
-					
-								// Make a POST Request to Messaging API to reply to sender
-								$url = 'https://api.line.me/v2/bot/message/reply';
-								$data = [
-									'replyToken' => $replyToken,
-									'messages' => [$messages3,$messages1]
-								];
-								$post = json_encode($data);
-								$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-					
-								$ch = curl_init($url);
-								curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-								curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-								curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-								curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-								curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-								$result = curl_exec($ch);
-								curl_close($ch);
-								#echo "check1";
-								#sleep(10);
-								#echo $result . "\r\n";
+								
+								
+								
+								
+								$sql="SELECT * FROM hoon_check";
+								
+								if ($result=mysqli_query($link,$sql))
+								  {
+								  // Fetch one and one row
+								  while ($row=mysqli_fetch_row($result))
+									{
+										$id_hoon_check=row[0]
+										$hoonname_check=row[1]
+										$timeframe_check=row[2]
+										$room_check=row[2]
+
+									}
+								  // Free result set
+								  mysqli_free_result($result);
+								}
+								
+								if($hoonname == $hoonname_check && $timeframe == $timeframe_check && $room == $room_check)
+								{
+									
+										$messages11 = ['type' => 'text','text' => "กราฟส่งไม่สมบูรณ์ บอทรีเช็ตใช้เวลา 5 นาที รบกวนเรียกใหม่อีกครั้งหลังจากนี้ 5 นาทีครับ"];
+										$url = 'https://api.line.me/v2/bot/message/reply';
+										$data = [
+											'replyToken' => $replyToken,
+											'messages' => [$messages11]
+										];
+										$post = json_encode($data);
+										$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+							
+										$ch = curl_init($url);
+										curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+										curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+										curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+										curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+										curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+										$result = curl_exec($ch);
+										curl_close($ch);
+										
+										
+										$sql = "DELETE FROM `check_capture` WHERE id = $id_hoon_check";
+
+										if (mysqli_query($link, $sql)) {
+											echo "Record deleted successfully";
+										} else {
+											echo "Error deleting record: " . mysqli_error($conn);
+										}
+										
+										$sql1 = "DELETE FROM `hoon_check` WHERE id = $id_hoon_check";
+
+										if (mysqli_query($link, $sql1)) {
+											echo "Record deleted successfully";
+										} else {
+											echo "Error deleting record: " . mysqli_error($conn);
+										}
+										#echo "check1";
+										#sleep(10);
+										#echo $result . "\r\n";
+									
+								}
+								else
+								{
+										// Make a POST Request to Messaging API to reply to sender
+										$url = 'https://api.line.me/v2/bot/message/reply';
+										$data = [
+											'replyToken' => $replyToken,
+											'messages' => [$messages3,$messages1]
+										];
+										$post = json_encode($data);
+										$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+							
+										$ch = curl_init($url);
+										curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+										curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+										curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+										curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+										curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+										$result = curl_exec($ch);
+										curl_close($ch);
+										#echo "check1";
+										#sleep(10);
+										#echo $result . "\r\n";
+								}
 							}
 						}
 						else
