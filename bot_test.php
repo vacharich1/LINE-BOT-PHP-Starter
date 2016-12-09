a2ed2b13200f076395ca8428125c25dd8f04166c
jay <jr30149_@hotmail.com>
2016-12-09 22:00:30 +0700

    sksksksk

--- bot_test.php
+++ bot_test.php
@@ -1668,7 +1668,7 @@ if (!is_null($events['events'])) {
 					if($text == '@@addgroupbyjay_botroom')
 					{
 							$replyToken = $event['replyToken'];
-							$messages55 = ['type' => 'text','text' => $event['source']['groupId']];
+							$messages55 = ['type' => 'text','text' => $event['source']['userId']];
 							// Make a POST Request to Messaging API to reply to sender
 							$url = 'https://api.line.me/v2/bot/message/reply';
 							$data = [
