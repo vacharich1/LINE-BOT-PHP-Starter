103aa84e565b235eac8198ae135493a33769339a
vacharich1 <jr30149_@hotmail.com>
2017-02-03 15:00:42 +0700

    Update bot_test.php

--- bot_test.php
+++ bot_test.php
@@ -1598,17 +1598,17 @@ if (!is_null($events['events'])) {
 										'replyToken' => $replyToken,
 										'messages' => [$messages1]
 									];
-									$post = json_encode($data);
-									$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+									//$post = json_encode($data);
+									//$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 						
-									$ch = curl_init($url);
-									curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-									curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-									curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-									curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-									curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-									$result = curl_exec($ch);
-									curl_close($ch);
+									//$ch = curl_init($url);
+									//curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+									//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+									//curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+									//curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+									//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+									//$result = curl_exec($ch);
+									//curl_close($ch);
 								}
 								
 								
